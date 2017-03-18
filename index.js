0ce6824ce24b6a5efa9efcdd6a71eab437479a3a
Lucas Zhou <lucasxz@outlook.com>
2017-03-18 14:50:50 -0700

    usernames added
    added prompt for usernames. now passes usernames from client to server

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-	});
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+	socket.on('chat message', function(msg, username){
+		io.emit('chat message', msg, username);
 	});
 });
 
