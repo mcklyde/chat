a6ebc98e2e22e20a7e3cd48d0654d24988622569
Lucas Zhou <lucasxz@outlook.com>
2017-03-19 12:18:10 -0700

    better rooms
    mcklyde idk how yours even works but it looks like it'll fk something up
    later on so lets do it this way

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/html/index.html');
+    res.sendFile(__dirname + '/html/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log('User ' + socket.id + ' connected');
-	socket.on('disconnect', function(){
-		console.log('User ' + socket.id + ' disconnected');
-	});
-	socket.on('chat message', function(msg, username){
-		io.emit('chat message', msg);
-	});
+    console.log('User ' + socket.id + ' connected');
+    socket.on('disconnect', function(){
+        console.log('User ' + socket.id + ' disconnected');
+    });
+    socket.on('room', function(roomid) {
+        socket.join(roomid, function() {
+            console.log(socket.id + ' has joined a room ' + roomid);
+            io.to('roomid', 'a new user has joined');
+        });
+
+    });
+    socket.on('chat message', function(data){
+        io.in(data.roomId).emit('chat message', data.msg);
+    });
 });
 
 http.listen(3000, function(){
-	console.log('server listening on *:3000');
+    console.log('server listening on *:3000');
 });
